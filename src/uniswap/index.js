ef7f22882a7eeea5b5bc772943c67dc5e63059bf
BOBO <2274580253@qq.com>
2021-05-17 10:58:05 +0800

    初始化项目

---
+++ src/uniswap/index.js
@@ -0,0 +1,9 @@
+
+'use strict'
+
+if (process.env.NODE_ENV === 'production') {
+  // module.exports = require('./sdk.cjs.production.min.js')
+  module.exports = require('./sdk.cjs.development.js')
+} else {
+  module.exports = require('./sdk.cjs.development.js')
+}
